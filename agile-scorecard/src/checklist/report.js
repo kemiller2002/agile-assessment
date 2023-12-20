5716471f16ae271bdf0d1721f11d02f309fa9c63
Kevin Miller <kemiller2002@gmail.com>
2023-12-20 16:54:20 -0500

    fix css issue report.css getting in way

--- agile-scorecard/src/checklist/report.js
+++ agile-scorecard/src/checklist/report.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Checklist } from "./checklist";
 import "./css/main.css";
-import "./css/report.css";
 
 export function Report(props) {
   return <Checklist http={props.http} disabled={true} />;
